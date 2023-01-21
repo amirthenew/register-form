604597b412ae049a8e82a242a4547803d736f03f
amirthenew <ah.abshari@hotmail.com>
2023-01-22 00:03:40 +0330

    finish ! (update to router 6)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import SignUp from './SignUp';
 import Login from './Login';
-import { Route,Switch,Redirect } from 'react-router-dom';
+import { Route,Routes,Navigate } from 'react-router-dom';
 import './App.css'
 
 function App() {
   return (
     <div className='page'>
-<Switch>
-<Route path='/login' component={Login} />
-<Route path='/signup' component={SignUp} />
-<Redirect from='/' to='signup'/>
-</Switch>
+<Routes>
+<Route path='/login' element={<Login/>} />
+<Route path='/signup' element={<SignUp/>} />
+<Route path='/' element={<Navigate to='/signup'/>} />
+</Routes>
 
  
     </div>
